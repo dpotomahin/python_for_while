db9322f97b7f2003d9304da3aa5d13fd2497c97a
Nick <92036651+dpotomahin@users.noreply.github.com>
2023-01-10 10:36:19 +0200

    Add files via upload

---
+++ for_while.py
@@ -0,0 +1,740 @@
+#task 1
+# v = 1;
+# for i in range(7):
+#     a = input('Enter number: ');
+#     if a < 6:
+#         v = v * a;
+# print(v)
+
+
+#task 2
+# v = 0;
+# for i in range(10):
+#     a = input('Enter number: ');
+#     if a > 0 and a % 2 == 0:
+#         v = v + 1;
+# print(v)
+
+
+#task 3
+# v = 0;
+# for i in range(6):
+#     a = input('Enter number: ');
+#     if a < 0 and a % 2 != 0:
+#         v = v + a;
+# print(v)
+
+
+
+#task 4
+# min = 10**10;
+# max = -10**10;
+# for i in range(5):
+#     a = input('Enter number: ');
+#     if a < min:
+#         min = a;
+#     if a > max:
+#         max = a;
+# print(min);
+# print(max);
+
+
+#task 5
+# s = 0;
+# k = 0;
+# for i in range(8):
+#     a = input('Enter number: ');
+#     if a != 0:
+#         k = k + 1;
+#         s = (s + a)
+# s = s/k
+# print(s)
+
+
+#task 6
+# s = 0;
+# for i in range(9):
+#     a = input('Enter number: ');
+#     if a < 5:
+#         s = s + a;
+# print(s)
+
+
+#task 7
+# s = 0;
+# k = 0;
+# for i in range(6):
+#     a = input('Enter number: ');
+#     if  i < 1:
+#         b = a;
+#     if a > b:
+#         k += 1;
+# print(k)
+
+
+#task 8
+# s = 1;
+# for i in range(6):
+#     a = input('Enter number: ');
+#     if 6 >= a >= 3:
+#         s = s * a;
+# print(s)
+
+
+#task 9
+# s = 0;
+# k = 0;
+# for i in range(8):
+#     a = input('Enter number: ');
+#     if a > 0:
+#         k = k + 1;
+#         s = (s + a)
+# s = s/k
+# print(s)
+
+
+
+#task 10
+# s = 0;
+# for i in range(8):
+#     a = input('Enter number: ');
+#     if a <= 3 and -a <= 3:
+#         s = (s + a**2)
+# print(s)
+
+
+#task 11
+# s = 0;
+# for i in range(14):
+#     a = input('Enter number: ');
+#     if a != 0:
+#         s = s + 1;
+# print(s)
+
+
+#task 12
+# s = 0;
+# m = 10**10;
+# for i in range(9):
+#     a = input('Enter number: ');
+#     s = s + 1;
+#     if a < m:
+#         m = a;
+#         k = s;
+# print(k)
+
+
+#task 13
+# s = 1;
+# for i in range(6):
+#     a = input('Enter number: ');
+#     if a != 0:
+#         s = s * a;
+# print(s)
+
+
+#task 14
+# s = 0;
+# k = 0;
+# for i in range(8):
+#     a = input('Enter number: ');
+#     if 10 <= a <= 20:
+#         k = k + 1;
+#         s = (s + a)
+# s = s/k
+# print(s)
+
+
+
+#task 15
+# k = 0;
+# for i in range(8):
+#     a = input('Enter number: ');
+#     if 5 <= a <= 10:
+#         k = k + 1;
+# print(k)
+
+
+#task 16
+# s = 0;
+# for i in range(7):
+#     a = input('Enter number: ');
+#     if a < 0:
+#         s = (s + (-a))
+# print(s)
+
+
+#task 17
+# s = 1;
+# for i in range(7):
+#     a = input('Enter number: ');
+#     if 0 < a <= 4:
+#         s = (s * a)
+# print(s)
+
+
+#task 18
+# k = 0;
+# m = 0;
+# for i in range(12):
+#     a = input('Enter number: ');
+#     if a < 0:
+#         k = k + 1;
+#     if a > 0:
+#         m = m + 1;
+# print(m);
+# print(k);
+
+
+#task 19
+# s = 0;
+# k = 0;
+# for i in range(8):
+#     a = input('Enter number: ');
+#     k = k + 1;
+#     if a < 0:
+#         s = (s + (-a));
+#     else:
+#         s = (s + a);
+# s = s/k
+# print(s)
+
+
+#task 20
+# min = 10**10;
+# max = -10**10;
+# for i in range(6):
+#     a = input('Enter number: ');
+#     if a < min:
+#         min = a;
+#     if a > max:
+#         max = a;
+# print(max - min);
+
+
+#task 21
+# s = 0;
+# for i in range(11):
+#     a = input('Enter number: ');
+#     if -10 >= a or a >= 10:
+#         s = s + a;
+# print(s);
+
+#task 22
+# s = 1;
+# for i in range(9):
+#     a = input('Enter number: ');
+#     if a % 2 != 0:
+#         s = s * a;
+# print(s);
+
+
+#task 23
+# s = 0;
+# for i in range(14):
+#     a = input('Enter number: ');
+#     if a % 3 == 0:
+#         s = s + 1;
+# print(s)
+
+#task 23/2
+# s = 0;
+# k = 0;
+# for i in range(8):
+#     a = input('Enter number: ');
+#     if a % 2 == 0:
+#         s = (s + a);
+#         k = k + 1;
+# s = s/k
+# print(s)
+
+
+#task 24
+# s = 0;
+# for i in range(6):
+#     a = input('Enter number: ');
+#     if  i < 1:
+#         b = a;
+#     if a < b:
+#         s += a;
+# print(s)
+
+
+#task 25
+# s = 1;
+# for i in range(9):
+#     a = input('Enter number: ');
+#     if -10 < a <10:
+#         s *= a;
+# print(s);
+
+
+#task 26
+# s = 10**10
+# for i in range(8):
+#     a = input('Enter number: ');
+#     if a % 2 != 0:
+#         if a < s:
+#             s = a;
+# print(s);
+
+
+#task 26/2
+# s = 0;
+# k = 0;
+# for i in range(14):
+#     a = input('Enter number: ');
+#     if a % 3 == 0:
+#         s = (s + a);
+#         k += 1;
+# s = s/k
+# print(s)
+
+
+#task 27
+# s = 0;
+# for i in range(7):
+#     a = input('Enter number: ');
+#     if a % 5 == 0:
+#         s *= a;
+# print(s)
+
+
+#task 27/2
+# s = -10**10
+# for i in range(10):
+#     a = input('Enter number: ');
+#     if a % 2 == 0 and a > 0:
+#         if a > s:
+#             s = a;
+# print(s);
+
+
+#task 28
+# s = 1;
+# for i in range(6):
+#     a = input('Enter number: ');
+#     if 3 <= a <= 6:
+#         s *= a;
+# print(s);
+
+
+#task 29
+# s = 0;
+# k = 0;
+# for i in range(8):
+#     a = input('Enter number: ');
+#     if a > 0:
+#         s = (s + a);
+#         k += 1;
+# s = s/k
+# print(s)
+
+
+#task 30
+# s = 0;
+# for i in range(8):
+#     a = input('Enter number: ');
+#     if a <= 3 and -a <= 3:
+#         s += a**2;
+# print(s)
+
+# ///////_WHILE_///////
+#task 1
+# s = 0;
+# i = 0;
+# while i < 6:
+#     a = input('Enter number: ');
+#     if i < 1:
+#         b = a;
+#     if a > b:
+#         s += 1;
+#     i += 1;
+# print(s)
+
+
+#task 2
+# s = 0;
+# i = 0;
+# while i < 6:
+#     a = input('Enter number: ');
+#     if  i < 1:
+#         b = a;
+#     if a < b:
+#         s += a;
+#     i += 1;
+# print(s)
+
+
+#task 3
+# s = 0;
+# i = 0;
+# while i < 6:
+#     a = input('Enter number: ');
+#     if i < 1:
+#         b = a;
+#     if a < b:
+#         b = a;
+#         k = True;
+#     else:
+#         k = False
+#     i += 1;
+# if k == True:
+#     print('True');
+# else:
+#     print('False')
+
+
+#task 4
+# i = 0;
+# b = 0
+# k = 10**10;
+# while i < 6:
+#     a = input('Enter number: ');
+#     if (b + a) < k:
+#         k = (b + a);
+#     b = a;
+#     i += 1;
+# print(k)
+
+
+#task 5
+# i = 0;
+# k = 10**10;
+# while i < 6:
+#     a = input('Enter number: ');
+#     if i < 1:
+#         b = a;
+#     if a < k:
+#         k = a
+#     i += 1;
+# s = (b - k)
+# print(s)
+
+
+# #task 6
+# i = 0;
+# max = -10**10;
+# min = 10**10;
+# while i < 6:
+#     a = input('Enter number: ');
+#     i += 1;
+#     if i % 2 != 0:
+#         if a < min:
+#             min = a;
+#     if i % 2 == 0:
+#         if a > max:
+#             max = a;
+# print(min + max)
+
+
+
+#task 7
+# i = 0;
+# b = 0
+# k = -10**10;
+# while i < 6:
+#     i += 1;
+#     a = input('Enter number: ');
+#     if i > 1:
+#         c = (b - a);
+#         if c < 0:
+#             c = -(b - a)
+#         if c > k:
+#             k = c;
+#     b = a;
+# print(k)
+
+
+#task 8
+# i = 0;
+# k = -10**10;
+# while i < 6:
+#     a = input('Enter number: ');
+#     if i < 1:
+#         b = a;
+#     if a > k:
+#         k = a
+#     i += 1;
+# s = (b - k)
+# print(s)
+
+
+#task 9
+# i = 0;
+# b = 0
+# s = 0
+# while i < 6:
+#     i += 1;
+#     a = input('Enter number: ');
+#     if i > 1:
+#         s = s + (b * a);
+#     b = a;
+# print(s)
+
+
+#task 10
+# i = 0;
+# b = 0
+# s = 1
+# while i < 6:
+#     i += 1;
+#     a = input('Enter number: ');
+#     if i > 1:
+#         s = s * (a - b);
+#     b = a;
+# print(s)
+
+
+
+#task 11
+# i = 0;
+# k = 0;
+# s = 0;
+# while i < 6:
+#     a = input('Enter number: ');
+#     if i < 1:
+#         b = a;
+#     if a < b:
+#         k += 1;
+#         s = s + a;
+#     i += 1;
+# s = s/k
+# print(s)
+
+
+#task 12
+# i = 0;
+# s = False;
+# while i < 6:
+#     i += 1;
+#     a = input('Enter number: ');
+#     if i > 1:
+#         if b == a:
+#            s = True;
+#     b = a;
+# if s:
+#     print('Yes')
+
+
+#task 13
+# s = 0;
+# i = 0;
+# while i < 6:
+#     a = input('Enter number: ');
+#     if i < 1:
+#         b = a;
+#     if a > b:
+#         b = a;
+#         k = True;
+#     else:
+#         k = False
+#     i += 1;
+# if k == True:
+#     print('True');
+# else:
+#     print('False')
+
+
+#task 14
+# min = 10**10;
+# max = -10**10;
+# i = 0
+# while i < 6:
+#     a = input('Enter number: ');
+#     if a < min:
+#         min = a;
+#     if a > max:
+#         max = a;
+#     i += 1;
+# print(max - min);
+
+
+#task 15
+# i = 0;
+# s = 0;
+# while i < 6:
+#     a = input('Enter number: ');
+#     if (a % 5 == 0) and (a % 7 != 0):
+#         s += a;
+#     i += 1;
+# print(s);
+
+
+#task 16
+# i = 0;
+# s = 0;
+# while i < 6:
+#     a = input('Enter number: ');
+#     if a > 0:
+#         s += a;
+#     i += 1;
+# print(s*2);
+
+
+#task 17
+# i = 0;
+# s = 0;
+# k = 0;
+# while i < 6:
+#     a = input('Enter number: ');
+#     if a < 0:
+#         s += a;
+#     else:
+#         k += 1;
+#     i += 1;
+# print(s);
+# print(k);
+
+
+#task 18
+# i = 0;
+# s = 0;
+# k = 0;
+# while i < 6:
+#     a = input('Enter number: ');
+#     if a < 0:
+#         s += 1;
+#     elif a > 0:
+#         k += 1;
+#     i += 1;
+# s = (s / 100) * 6;
+# # k = (k / 6) * 100;
+# # d = 100 - (s + k);
+# print(str(s) + '% < 0');
+# # print(str(k) + '% > 0');
+# # print(str(d) + '% = 0');
+
+
+
+#task 20
+# i = 0;
+# s = False;
+# b = ''
+# while i < 6:
+#     i += 1;
+#     a = input('Enter number: ');
+#     if b == a:
+#         s = True;
+#     if i == 1:
+#         b = a;
+# if s:
+#     print('Yes')
+
+
+
+#task 21
+# i = 0;
+# while i < 6:
+#     i += 1;
+#     a = input('Enter number: ');
+#     if a == 0:
+#        k = i;
+# print(k)
+
+
+#task 22
+# i = 0;
+# k = 0
+# while i < 6:
+#     a = input('Enter number: ');
+#     i += 1;
+#     if i % 2 == 0:
+#         if a % 2 != 0:
+#             k += 1
+# print(k)
+
+
+#task 23
+# i = 0;
+# s = 0;
+# while i < 6:
+#     i += 1;
+#     a = input('Enter number: ');
+#     if i == 1:
+#         b = a;
+#     if a < b:
+#         s = a**2 + s;
+# print(s)
+
+
+#task 24
+# i = 0;
+# s = 0;
+# while i < 6:
+#     i += 1;
+#     a = input('Enter number: ');
+#     if (a >= 100 and a < 1000) or (-a >= 100 and -a < 1000):
+#         s += 1;
+# print(s)
+
+
+#task 25
+# i = 0;
+# s = 0;
+# while i < 6:
+#     i += 1;
+#     a = input('Enter number: ');
+#     if a > 0:
+#         s += a;
+#     else:
+#         print(s)
+
+
+
+#task 26
+# i = 0;
+# s = 0;
+# while i < 6:
+#     i += 1;
+#     a = input('Enter number: ');
+#     if i == 1:
+#         b = a;
+#     if b - a > 10 or b - a < -10:
+#         s += 1;
+# print(s)
+
+
+#task 27
+# i = 0;
+# s = 0;
+# while i < 6:
+#     i += 1;
+#     a = input('Enter number: ');
+#     if a != 0:
+#         s += a;
+#     else:
+#         print(s)
+
+
+#task 28
+# i = 0
+# s = 0
+# while i < 6:
+#     i += 1;
+#     a = input('Enter number: ');
+#     if a % 2 != 0:
+#         s += 1;
+# print(s)
+
+
+#task 29
+# i = 0
+# while i < 6:
+#     i += 1;
+#     a = input('Enter number: ');
+#     if a < 0:
+#         s = a;
+# print(s)
+
+
+
+#task 30
+# i = 0;
+# s = 0;
+# while i < 6:
+#     i += 1;
+#     a = input('Enter number: ');
+#     if (a >= 10 and a < 100) or (-a >= 10 and -a < 100):
+#         s += 1;
+# print(s)
